645328b3a3c72122af21f0699f22c10d4f967b48
levchakovd <108088698+levchakovd@users.noreply.github.com>
2022-10-17 23:37:02 +0700

    Update 3_2.py
    3_2 done

--- lesson3/3_2.py
+++ lesson3/3_2.py
@@ -10,3 +10,22 @@
 #out
 #[2, 2, 4, 8, 8]
 #[16, 16, 4]
+
+from random import randint
+
+
+num = int(input('введите число '))
+num_list = []
+for i in range(num):
+    num_list.append(randint(-10, 10))
+
+print(num_list)
+
+num_list_result = []
+
+for i in range((len(num_list)+1)//2):
+    if (i != (len(num_list)-1-i)):
+        num_list_result.append(num_list[i]*num_list[len(num_list)-1-i])
+    else:
+        num_list_result.append(num_list[i])
+print(num_list_result)
