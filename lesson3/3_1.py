71d71552f4e042e37ba8a2907e5ae835d8aefa80
levchakovd <108088698+levchakovd@users.noreply.github.com>
2022-10-17 23:49:57 +0700

    Update 3_1.py
    3_1 done

--- lesson3/3_1.py
+++ lesson3/3_1.py
@@ -4,3 +4,20 @@
 # out
 #[10, 2, 3, 8, 9]
 #22
+
+from random import randint
+
+num = int(input('введите число '))
+num_list = []
+for i in range(num):
+    num_list.append(randint(-10, 10))
+
+print(num_list)
+
+result = 0
+
+for i in range(0, num, 2):
+    result += num_list[i]
+
+print(result)
+
