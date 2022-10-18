30e51fe2060394eb72658cf4c931fa9eaecd4afa
levchakovd <108088698+levchakovd@users.noreply.github.com>
2022-10-18 23:31:44 +0700

    Update 3_3.py
    done

--- lesson3/3_3.py
+++ lesson3/3_3.py
@@ -13,4 +13,15 @@
 #print('{0:b}'.format(a))
 #type = str - не подходит значит(
 
-a = int(input('Введите число, чтобы я преобразовал его в двоичное: '))
+
+
+def conv_bin(num: int):
+    list_nums = []
+
+    while num > 0:
+        list_nums.insert(0, num%2)
+        num //=2
+
+    print(*list_nums, sep='')
+
+conv_bin(int(input('Введите число, чтобы я преобразовал его в двоичное: ')))
