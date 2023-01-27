b6ac83465ce28a7d85f91f735ab1a975c4c91b05
levchakovd <108088698+levchakovd@users.noreply.github.com>
2023-01-26 23:28:44 -0300

    Create 4_1.py

---
+++ lesson4/4_1.py
@@ -0,0 +1,20 @@
+#1. Вычислить число c заданной точностью d
+#in
+#Enter a real number: 9
+#Enter the required accuracy '0.0001': 0.000001
+
+#out
+#9.000000
+
+#in
+#Enter a real number: 8.98785
+#Enter the required accuracy '0.0001': 0.001
+
+#out
+#8.988
+
+n = float(input('Enter a real number: '))
+b = input('Enter the required accuracy: ')
+
+print(n)
+
